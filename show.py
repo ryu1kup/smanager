5b2a40368dac132d1256b6c6eadae688fe15027a
Ryuichi Ueno <ryu1@stu.kobe-u.ac.jp>
2020-04-24 03:27:29 -0500

    [update] show.py

--- show.py
+++ show.py
@@ -1,4 +1,17 @@
+import argparse
+
 from smanager import smanager
 
-manager = smanager(user='ryuichi')
-manager.show()
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--user', type=str, default='')
+    args = parser.parse_args()
+    return args
+
+def show(user):
+    manager = smanager(user=user)
+    manager.show()
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    show(args.user)
